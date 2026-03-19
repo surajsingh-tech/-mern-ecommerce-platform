212d3080930b655fef9110dab126badf4f2b343a
Suraj Singh Bangari <your-email@example.com>
2026-03-19 23:56:45 +0530

    Backend updates: added CORS support, updated package files and server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
-import express from 'express';
-import 'dotenv/config';
-import connectDB from './database/db.js';
-import userRoute from './routes/userRoute.js';
+import express from "express";
+import "dotenv/config";
+import connectDB from "./database/db.js";
+import userRoute from "./routes/userRoute.js";
+import cors from "cors";
 
-const app =express();
+const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use('/api/v1/user',userRoute)
-app.listen(PORT,()=>{
-    connectDB()
-    console.log(`Server is listen at PORT  http://localhost:${PORT}`);
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+app.use("/api/v1/user", userRoute);
 
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is listen at PORT  http://localhost:${PORT}`);
+});
